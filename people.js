1c8a3b50bd0438c0ab8b39b0ba5d06e90dd0e71a
Allan Mwenja <mwenjaallan18@gmail.com>
2025-09-04 09:35:19 +0300

    JS files

---
+++ people.js
@@ -0,0 +1,10 @@
+const people = ['yoshi', 'ryu','chin','guy'];
+const ages = [20,25,30,35];
+
+console.log(people);
+
+module.exports = {
+    people: people, 
+    ages:ages
+    //people, ages
+};
