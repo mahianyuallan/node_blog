ab7395b37e849f70087195c18fa2da58d5ebc626
Allan Mwenja <mwenjaallan18@gmail.com>
2025-09-04 09:32:59 +0300

    Update

---
+++ modules.js
@@ -0,0 +1,10 @@
+//const xyz = require('./people.js');
+//console.log(xyz.people, xyz.ages);
+
+const {people, ages} = require('./people.js');
+
+console.log(people, ages);
+
+const os = require('os');
+
+console.log(os.platform(), os.homedir());
